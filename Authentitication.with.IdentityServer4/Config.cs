c7a13bae95000e6dfea9e5ece92ae10ebad0df42
electrons500 <electrons500@gmail.com>
2023-06-02 14:44:25 +0000

    Add project files.

---
+++ Authentitication.with.IdentityServer4/Config.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Authentitication.with.IdentityServer4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        //define api scope
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> 
+            { 
+                new ApiScope("api1","My API")
+            };
+
+        /* define client application.We will authenticate clients 
+           using client secret.
+        */
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",   
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    //secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = {"api1"}
+
+                    //NB: ClientId and secret may be considered 
+                    //as the login and password the client app with use to connect
+                    // to the identityServer
+                }
+            };
+    }
+}
